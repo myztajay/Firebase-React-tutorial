9fc54d7bedfb0a3ad98eca8990d03704b619a95b
myztajay <myztajay@hotmail.com>
2017-05-07 16:13:20 -0400

    Initialized firebase and added it keys to git ignore

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Firebase } from './Firebase';
 import { Header } from './components/common';
 
+
 class App extends Component {
+
+  compoentWillMount(){
+    Firebase;
+  }
+
   render() {
     return (
       <View>
